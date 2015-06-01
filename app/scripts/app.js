5f21efe55ac28942f62df789beb12221a41ff665
Joshua Marquez N <josuehenry14@hotmail.com>
2015-06-01 12:37:01 -0700

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name josuehenrygithubioApp
+ * @description
+ * # josuehenrygithubioApp
+ *
+ * Main module of the application.
+ */
+angular.module('jmSiteApp', [
+  'ngAnimate',
+  'ngMessages',
+  'ngSanitize',
+  'ngTouch'
+]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name josuehenrygithubioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the josuehenrygithubioApp
+ */
+angular.module('jmSiteApp').controller('MainCtrl', function ($scope) {});
